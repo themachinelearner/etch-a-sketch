bc3ffb5b204e6ec4d540bf944ba9918e8bd410b0
Ben Smith <wiredcowboys@gmail.com>
2023-05-28 21:39:42 -0600

    Finish first cut of functioning program

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 function setUpNewGrid() {
+    const numCellsOnSide = getUserInput();
+    clearUserInput();
+    if (!numCellsOnSide) {
+        return;
+    }
+    clearGrid();
+    populateGrid(numCellsOnSide);
+}
+
+function getUserInput() {
     const input = document.querySelector('#sizeInput');
     if (!parseInt(input.value)) {
         return;
     }
+    return parseInt(input.value);
+}
 
+function clearUserInput() {
+    const input = document.querySelector('#sizeInput');
+    input.value = '';
+}
+
+function clearGrid() {
     const gridHolder = document.querySelector('#gridHolder');
     const outerChildren = gridHolder.children;
 
@@ -16,6 +34,24 @@ function setUpNewGrid() {
 
         outerElement.remove();
     });
+}
+
+function populateGrid(numCellsOnSide) {
+    const gridHolder = document.querySelector('#gridHolder');
+    for (let i = 0; i < numCellsOnSide; i++) {
+        const outerItem = document.createElement('div');
+        for (let j = 0; j < numCellsOnSide; j++) {
+            const innerItem = document.createElement('div');
+            innerItem.addEventListener('mouseover', darkenBox);
+            outerItem.appendChild(innerItem);
+        }
+        outerItem.addEventListener('mouseover', darkenBox);
+        gridHolder.appendChild(outerItem);
+    }
+}
+
+function darkenBox(e) {
+    e.target.classList.add('darkened');
 }
 
 const resetButton = document.querySelector('#resetButton');
